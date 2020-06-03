01c39407f6e4c27a284ef433372240d6467ef24b
greghowe79 <64290303+greghowe79@users.noreply.github.com>
2020-06-03 20:34:20 +0200

    Updates

---
+++ precache-manifest.57c43274de5c3becf2ebb15c774e481a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e710152884acfb88861a3c9a69fa4194",
+    "url": "/react-redux/index.html"
+  },
+  {
+    "revision": "0fbc3c95538adacc3480",
+    "url": "/react-redux/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e2a40dfa4133113bd05e",
+    "url": "/react-redux/static/css/main.b4e0380c.chunk.css"
+  },
+  {
+    "revision": "0fbc3c95538adacc3480",
+    "url": "/react-redux/static/js/2.c2073658.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux/static/js/2.c2073658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a40dfa4133113bd05e",
+    "url": "/react-redux/static/js/main.475a4842.chunk.js"
+  },
+  {
+    "revision": "94338a12fe5d9fd8933a",
+    "url": "/react-redux/static/js/runtime-main.c71757ab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
